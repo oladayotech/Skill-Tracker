6023fddaf86dbecc6258efadc07157610707c55c
oladayotech <d1arogundade@gmail.com>
2025-04-23 14:28:08 +0100

    Design homepage and styling it

--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -3,7 +3,7 @@ from  . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login', views.home, name='login'),
-    path('signup', views.home, name='signup'),
-    path('signup', views.home, name='signup'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('dashboard', views.home, name='dashboard'),
 ]
