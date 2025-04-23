91eb97fd2c1d3d4bbcee412e9b259fde0ea81be6
oladayotech <d1arogundade@gmail.com>
2025-04-23 12:29:54 +0100

    Design homepage and styling it

--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login', views.home, name='login'),
     path('signup', views.home, name='signup'),
+    path('signup', views.home, name='signup'),
 ]
