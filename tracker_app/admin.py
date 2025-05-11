5e96281e39bf25c210d78fc02c7f4d9b7c8fb162
oladayotech <d1arogundade@gmail.com>
2025-05-11 01:27:22 +0100

    update

--- tracker_app/admin.py
+++ tracker_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Journal
+from .models import Journal, DailyLog
 
 # Register your models here.
 admin.site.register(Journal)
+admin.site.register(DailyLog)
