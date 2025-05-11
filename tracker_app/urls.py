4c75f5585982760366a0a439b2a0fe223584f698
oladayotech <d1arogundade@gmail.com>
2025-05-12 00:46:47 +0100

    update

--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -3,11 +3,12 @@ from  . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('navbar', views.navbar, name='navbar'),
+    # path('navbar', views.navbar, name='navbar'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('signup/', views.user_signup, name='signup'),
     path('skills/', views.skills, name='skills'),
+    path('onboarding/', views.skills, name='onboarding'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dashboard/data/', views.dashboard_data, name='dashboard-data'),
     path('about/', views.about, name='about'),
